2f8b6001bb726fa3f145f0ad5a8d4b0881a0eb7a
Dizrtt <demonic2003@bk.ru>
2023-07-01 12:15:43 +0200

    Toy class  started.

---
+++ model/Toy.java
@@ -0,0 +1,58 @@
+package model;
+
+public class Toy {
+    int id;
+    String name;
+    int quantity;
+    int chance;
+
+    public String info() {
+        return "" + this.getName() +
+                "!!! под серийным номером " + this.getId() +
+                ". Вероятность выпадания такой игрушки составляет всего " + this.getChance() + 
+                "%!!! Всего таких " + this.getQuantity() +
+                " в наличии.";
+    }
+
+    public Toy() {
+    }
+
+    public Toy(int id, String name, int quantity, int chance) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.chance = chance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getChance() {
+        return chance;
+    }
+
+    public void setChance(int chance) {
+        this.chance = chance;
+    }
+}
